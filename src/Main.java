e2963e0a89a6109fc3d9edb716806e51e20f9262
NewSirius <newsirius@mail.ru>
2018-07-30 08:55:35 +0300

    Second commit from master

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Первая строка мастера");
+        System.out.println("Вторая строка мастера");
     }
 }
